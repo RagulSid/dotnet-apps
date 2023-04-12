3ae1d64e920e3f93d8d3a6ca3c3fbdef40d6a616
RagulSid <ragulsiddharth1@gmail.com>
2023-04-12 16:25:35 +0530

    first commit

---
+++ BankingApp/BankingApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;   //system is a namespace
+
+namespace BankingApp       //namespace is just like a folder for your classes
+{
+    class Program
+    {
+        static void Main(string[] args)          //entry point
+        {
+            var account = new BankAccount("Ragul", 10000);
+            Console.WriteLine($"Account {account.AcctNum} was created for {account.Owner} with {account.Balance} initial balance.");
+
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            Console.WriteLine(account.GetAccountHistory());
+        }
+    }
+}
